718e6492e4da7f467baa6ff5dc07b4d6186f92df
Marius Florescu <mariusflorescu23@icloud.com>
2021-10-27 00:50:11 +0300

    solved some bugs

--- components/input/index.js
+++ components/input/index.js
@@ -1,42 +1,54 @@
 import React from "react";
 import useStore from "../../context";
 import generateId from "../../lib/generateId";
-import { StyledInput } from "./styles";
+import { StyledInput, Small } from "./styles";
+
+const limit = 240;
 
 const Input = () => {
   const { add } = useStore();
   const [current, setCurrent] = React.useState("");
+  const [justAdded, setJustAdded] = React.useState(true);
   const [rows, setRows] = React.useState(1);
 
   const textareaRef = React.useRef(null);
 
   const handleKeyDown = (e) => {
     if (e.keyCode === 13 && !e.shiftKey) {
-      const obj = {
-        key: generateId(),
-        text: current,
-      };
-      add(obj);
-      setCurrent("");
-      setRows(1);
-    } else if (
-      e.keyCode === 13 &&
-      e.shiftKey &&
-      textareaRef &&
-      textareaRef.current
-    ) {
+      if (current.trim() !== "") {
+        const obj = {
+          key: generateId(),
+          text: current,
+        };
+        add(obj);
+        setCurrent("");
+        setRows(1);
+      } else {
+        setCurrent("");
+      }
+    } else if (e.keyCode === 13 && current.trim() !== "") {
       setRows((prev) => prev + 1);
     }
   };
+
   return (
-    <StyledInput
-      ref={textareaRef}
-      rows={rows}
-      value={current}
-      onChange={(e) => setCurrent(e.target.value)}
-      onKeyDown={(e) => handleKeyDown(e)}
-      placeholder="Write your thoughts..."
-    />
+    <React.Fragment>
+      <StyledInput
+        rows={1}
+        ref={textareaRef}
+        value={current}
+        onChange={(e) =>
+          e.target.value.trim() !== "" || current.trim() !== ""
+            ? setCurrent(e.target.value)
+            : null
+        }
+        onKeyDown={(e) => handleKeyDown(e)}
+        placeholder="Write your thoughts..."
+      />
+      <Small>
+        {current.length}/{limit}
+      </Small>
+    </React.Fragment>
   );
 };
 
--- components/input/styles.js
+++ components/input/styles.js
@@ -4,6 +4,25 @@ const StyledInput = styled("textarea", {
   all: "unset",
   width: "100%",
   marginX: "$1",
+  paddingY: "$3",
+  paddingX: "$2",
+  borderRadius: "$1",
+
+  "@motionSafe": {
+    transition: "250ms ease",
+    transitionProperty: "background-color",
+  },
+
+  "&:hover, &focus": {
+    backgroundColor: "$gray200",
+  },
 });
 
-export { StyledInput };
+const Small = styled("small", {
+  width: "100%",
+  color: "$gray600",
+  display: "flex",
+  justifyContent: "flex-end",
+});
+
+export { StyledInput, Small };
