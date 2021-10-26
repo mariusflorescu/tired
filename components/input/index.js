056366807c334084afb464c8758d37ef418e7c2c
Marius Florescu <mariusflorescu23@icloud.com>
2021-10-27 00:15:07 +0300

    added shift+enter

--- components/input/index.js
+++ components/input/index.js
@@ -6,19 +6,32 @@ import { StyledInput } from "./styles";
 const Input = () => {
   const { add } = useStore();
   const [current, setCurrent] = React.useState("");
+  const [rows, setRows] = React.useState(1);
+
+  const textareaRef = React.useRef(null);
 
   const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
+    if (e.keyCode === 13 && !e.shiftKey) {
       const obj = {
         key: generateId(),
         text: current,
       };
       add(obj);
       setCurrent("");
+      setRows(1);
+    } else if (
+      e.keyCode === 13 &&
+      e.shiftKey &&
+      textareaRef &&
+      textareaRef.current
+    ) {
+      setRows((prev) => prev + 1);
     }
   };
   return (
     <StyledInput
+      ref={textareaRef}
+      rows={rows}
       value={current}
       onChange={(e) => setCurrent(e.target.value)}
       onKeyDown={(e) => handleKeyDown(e)}
--- components/input/styles.js
+++ components/input/styles.js
@@ -1,7 +1,8 @@
 import { styled } from "../../stitches.config";
 
-const StyledInput = styled("input", {
+const StyledInput = styled("textarea", {
   all: "unset",
+  width: "100%",
   marginX: "$1",
 });
 
